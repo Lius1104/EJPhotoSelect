2cdcfff333d4967214dad287e7208f7e0d194b0a
ejiang <lius@LiuShuangdeMac-mini.local>
2020-04-14 10:33:38 +0800

    Update EJPhoto.m

--- EJPhotoSelect/PhotoBrowser/EJPhoto.m
+++ EJPhotoSelect/PhotoBrowser/EJPhoto.m
@@ -7,7 +7,6 @@
 //
 
 
-#import <AssetsLibrary/AssetsLibrary.h>
 #import "EJPhoto.h"
 #import "EJPhotoBrowser.h"
 #import <SDWebImage/SDWebImage.h>
@@ -175,12 +174,13 @@
     } else if (_photoURL) {
         
         // Check what type of url it is
-        if ([[[_photoURL scheme] lowercaseString] isEqualToString:@"assets-library"]) {
-            
-            // Load from assets library
-            [self _performLoadUnderlyingImageAndNotifyWithAssetsLibraryURL: _photoURL];
-            
-        } else if ([_photoURL isFileReferenceURL]) {
+//        if ([[[_photoURL scheme] lowercaseString] isEqualToString:@"assets-library"]) {
+//
+//            // Load from assets library
+//            [self _performLoadUnderlyingImageAndNotifyWithAssetsLibraryURL: _photoURL];
+//
+//        } else
+        if ([_photoURL isFileReferenceURL]) {
             
             // Load from local file async
             [self _performLoadUnderlyingImageAndNotifyWithLocalFileURL: _photoURL];
@@ -249,32 +249,32 @@
 }
 
 // Load from asset library async
-- (void)_performLoadUnderlyingImageAndNotifyWithAssetsLibraryURL:(NSURL *)url {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        @autoreleasepool {
-            @try {
-                ALAssetsLibrary *assetslibrary = [[ALAssetsLibrary alloc] init];
-                [assetslibrary assetForURL:url
-                               resultBlock:^(ALAsset *asset){
-                                   ALAssetRepresentation *rep = [asset defaultRepresentation];
-                                   CGImageRef iref = [rep fullScreenImage];
-                                   if (iref) {
-                                       self.underlyingImage = [UIImage imageWithCGImage:iref];
-                                   }
-                                   [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
-                               }
-                              failureBlock:^(NSError *error) {
-                                  self.underlyingImage = nil;
-                                  EJLog(@"Photo from asset library error: %@",error);
-                                  [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
-                              }];
-            } @catch (NSException *e) {
-                EJLog(@"Photo from asset library error: %@", e);
-                [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
-            }
-        }
-    });
-}
+//- (void)_performLoadUnderlyingImageAndNotifyWithAssetsLibraryURL:(NSURL *)url {
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        @autoreleasepool {
+//            @try {
+//                ALAssetsLibrary *assetslibrary = [[ALAssetsLibrary alloc] init];
+//                [assetslibrary assetForURL:url
+//                               resultBlock:^(ALAsset *asset){
+//                                   ALAssetRepresentation *rep = [asset defaultRepresentation];
+//                                   CGImageRef iref = [rep fullScreenImage];
+//                                   if (iref) {
+//                                       self.underlyingImage = [UIImage imageWithCGImage:iref];
+//                                   }
+//                                   [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+//                               }
+//                              failureBlock:^(NSError *error) {
+//                                  self.underlyingImage = nil;
+//                                  EJLog(@"Photo from asset library error: %@",error);
+//                                  [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+//                              }];
+//            } @catch (NSException *e) {
+//                EJLog(@"Photo from asset library error: %@", e);
+//                [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
+//            }
+//        }
+//    });
+//}
 
 // Load from photos library
 - (void)_performLoadUnderlyingImageAndNotifyWithAsset:(PHAsset *)asset targetSize:(CGSize)targetSize {
